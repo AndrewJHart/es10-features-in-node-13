9c4add352a7be10d72de377b0261c4c7890cd96f
Landon Hart <landonhart@Landons-Air.tx.rr.com>
2020-01-08 11:32:31 -0600

    remove original imports

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 // const max = require('./utils').maxValue;
 
-import { maxValue, random } from './utils.js';
-import { readFile } from 'fs';
-
 const nums = [1,2,4];
-
-console.log(maxValue(...nums));
-
 const _math = './utils.js';
 
 // must use --experimental-modules to use async import
